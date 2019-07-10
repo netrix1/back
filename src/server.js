f4be6939fd8d0a9ee3ce38058519ba2d999f3286
System Administrator <root@iMac-de-Everson.local>
2019-07-09 23:08:12 -0300

    estrutura Back

---
+++ src/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+
+mongoose.connect("mongodb+srv://netrix:netrix@cluster0-e1xjx.mongodb.net/netrix?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true
+  }
+);
+
+app.use((req, res, next) => {
+  req.io = io;
+
+  return next();
+});
+
+io.on("connection", socket => {
+  socket.on('connectRoom', box => {
+    socket.join(box);
+  })
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use('./files', express.static(path.resolve(__dirname, "..", "tmp")));
+app.use('/files', express.static(path.resolve(__dirname, "..", "tmp")));
+
+app.use(require("./routes"));
+
+server.listen(3333);
